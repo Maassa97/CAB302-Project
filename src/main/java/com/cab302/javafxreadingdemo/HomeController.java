899249dddc26dfb3dd34520077514f66015c4a12
hazelisstressed <hhoga6@hotmail.com>
2025-09-10 21:14:10 +1000

    add light mode!!

--- src/main/java/com/cab302/javafxreadingdemo/HomeController.java
+++ src/main/java/com/cab302/javafxreadingdemo/HomeController.java
@@ -1,6 +1,6 @@
 package com.cab302.javafxreadingdemo;
 
-import javafx.application.Platform;
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Bounds;
@@ -8,6 +8,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import atlantafx.base.theme.Dracula;
+//import atlantafx.base.theme.PrimerDark;
+import atlantafx.base.theme.CupertinoLight;
 
 public class HomeController {
 @FXML private StackPane rootStack;
@@ -27,4 +32,33 @@ public class HomeController {
             e.printStackTrace();
         }
     }
+
+    //toggle
+
+    @FXML private ToggleButton lightBtn;
+    @FXML private ToggleButton darkBtn;
+
+    @FXML
+    private void initialize() {
+        ToggleGroup themeGroup = new ToggleGroup();
+        lightBtn.setToggleGroup(themeGroup);
+        darkBtn.setToggleGroup(themeGroup);
+
+        themeGroup.selectedToggleProperty().addListener((obs, oldT, newT) -> {
+            if (newT == null) return;
+            ToggleButton btn = (ToggleButton) newT;
+            if ("Light".equals(btn.getText())) {
+                Application.setUserAgentStylesheet(
+                        new CupertinoLight().getUserAgentStylesheet()
+                );
+            } else {
+                Application.setUserAgentStylesheet(
+                        new Dracula().getUserAgentStylesheet()
+                );
+            }
+        });
+    }
+
+
 }
+
