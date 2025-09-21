0300812a1d52a3424538836b1490fefebc312ec5
AnnaKabanna <Anna.Lanigan@connect.qut.edu.au>
2025-09-21 10:33:32 +1000

    Current to do list

--- src/main/java/com/example/todolistprototype/HelloApplication.java
+++ src/main/java/com/example/todolistprototype/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 420, 520  );
         stage.setTitle("To do list!");
         stage.setScene(scene);
         stage.show();
