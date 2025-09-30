41ffd54837da69430dfff8485fae543fa83ccc76
hazelisstressed <hhoga6@hotmail.com>
2025-09-30 22:15:01 +1000

    commit models before intellij crashes

---
+++ src/main/java/com/cab302/javafxreadingdemo/model/Deck.java
@@ -0,0 +1,9 @@
+package com.cab302.javafxreadingdemo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class FlashcardDeck {
+
+}
---
+++ src/main/java/com/cab302/javafxreadingdemo/model/Flashcard.java
@@ -0,0 +1,4 @@
+package com.cab302.javafxreadingdemo.model;
+
+public class Flashcard {
+}
